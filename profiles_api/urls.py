ade67da3110f06a167ef92f4bfbd03732d57aec5
Memo <hyperhorus@gmail.com>
2021-10-23 21:02:10 -0500

    Added POST, PUT, PATCH and DELETE methods to our APIView

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from profiles_api import views
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view())
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('my-view/', views.MyTestView.as_view())
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializer
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializer.HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of APIView features"""
@@ -13,4 +17,45 @@ class HelloApiView(APIView):
                       ]
         return Response({'message':'Hello!', 'an_apiview':an_apiview})
 
+
+    def post(self, request):
+        """Create Hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of the object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
+
+
+
 # Create your views here.
+class MyTestView(APIView):
+    """This is my own test"""
+
+    def get(self, request, format=None):
+        """return something here"""
+        my_view = [
+            'This is a test of my own',
+            'Quiero ver que funciones de otra forma',
+            'para ir aprendiendo mas y mas'
+
+                   ]
+        return Response({'mensaje':'Mensaje de Weri', 'una vista':my_view})
